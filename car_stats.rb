d0ef988168d31ab7927d3a0bdc986c119add9346
Paul Dombkowski <pdombkowski@gmail.com>
2014-06-05 11:21:28 -0500

    Cars lesson in class initial commit. Completed for day 1

--- car_stats.rb
+++ car_stats.rb
@@ -1,3 +1,22 @@
+
 class CarStats
-  # TODO
+  def self.calc_top_color(cars)
+    cars = cars.map { |car| car.color }
+    histogram = cars.inject(Hash.new(0)) do |r, e|
+      r[e] += 1
+      r
+    end
+
+    histogram.max_by { |cars,count| count }.first
+  end
+
+  def self.calc_bottom_color(cars)
+    cars = cars.map { |car| car.color }
+    histogram = cars.inject(Hash.new(0)) do |r, e|
+      r[e] += 1
+      r
+    end
+
+    histogram.min_by { |cars,count| count }.first
+  end
 end
