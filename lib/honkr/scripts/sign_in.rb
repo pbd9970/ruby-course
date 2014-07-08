637631834e2db331f756f7b9170f1f512aa50b2b
Paul Dombkowski <pdombkowski@gmail.com>
2014-07-07 19:32:31 -0500

    Pulled sign_in.rb from honkr branch

--- lib/honkr/scripts/sign_in.rb
+++ lib/honkr/scripts/sign_in.rb
@@ -2,8 +2,14 @@ module Honkr
   class SignIn
 
     def self.run(params)
-      # TODO
+      user = Honkr.db.get_user_by_username(params[:username])
+      result = OpenStruct.new(:success? => user.has_password?(params[:password]))
+      if result.success?
+        result.session_id = Honkr.db.create_session({user_id: user.id})
+      else
+        result.error = :invalid_password 
+      end
+      result
     end
-
   end
 end
